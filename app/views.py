54cdd5be317e94b435cd57c0807b05eef7e2b348
André Alves <andr.alves@ua.pt>
2020-11-11 23:34:46 +0000

    altered template html and added meteorology images

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from datetime import datetime
+
 
 # Create your views here.
+def home(request):
+    tparams = {
+        'title': 'Home Page',
+        'year': datetime.now().year,
+    }
+    return render(request, 'index.html', tparams)
