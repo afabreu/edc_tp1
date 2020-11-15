a1092e0ed8427e8a78c2024cd69ec7ae46c44d66
andre <almeida.a@code.ua.pt>
2020-11-15 20:55:43 +0000

    Minor changes
    Signed-off-by: andre <almeida.a@code.ua.pt>

--- app/basex_actions.py
+++ app/basex_actions.py
@@ -3,7 +3,6 @@ from lxml import etree
 from datetime import timedelta
 import xmltodict
 from datetime import datetime, timedelta
-import pandas as pd
 
 
 def db_to_xml(city_name: str,
@@ -63,39 +62,3 @@ def db_to_xml(city_name: str,
 
     return info
 
-
-def update_db(bd_name: str, xml: str):
-    """
-    TODO maybe change the function's name to something more specific,
-        if more xupdate functions are added
-    :param bd_name: Name of the database
-    :param xml: xml coming from the api
-    :return: Updates the db
-    """
-
-    session = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
-    try:
-        # TODO the required update
-        ...
-    finally:
-        session.close()
-
-
-def update_city(root: etree.Element, city: str):
-    """
-
-    :param root: xml element
-    :param city: city name
-    :return: modifies database (xupdate)
-    """
-    xml = root_to_xml(root)
-    ...
-
-
-def root_to_xml(root: etree.Element) -> str:
-    """
-
-    :param root: root of xml
-    :return: whole xml as string
-    """
-    ...
