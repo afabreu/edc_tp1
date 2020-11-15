173e81a17e2516d700d63a779ab56b4b40c46b93
andre <almeida.a@code.ua.pt>
2020-11-15 18:16:03 +0000

    Minor changes
    Signed-off-by: andre <almeida.a@code.ua.pt>

--- app/basex_actions.py
+++ app/basex_actions.py
@@ -8,22 +8,24 @@ import pandas as pd
 
 def db_to_xml(city_name: str,
               db_name: str = "FiveDaysForecast",
-              date: datetime = datetime.now()) -> dict:
+              date: datetime = datetime.now(),
+              is_forecast: bool = False) -> dict:
     """
     TODO
     :param db_name: name of database containing the data
     :param city_name: name of the city
     :param date: day of requested weather info
+    :param is_forecast: True if it is a forecast
     :return: xml with city's weather info: '<time...>...</time>'
     """
 
     assert date is datetime, "date should be datetime"
-    is_forecast = False
 
     session = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
     try:
         # TODO check if city is in db
         ...
+
         # if so Select city
         city_xml = session.execute("xquery collection('{}')//weatherdata[location/name='{}']"
                                    .format(db_name, city_name))
@@ -39,11 +41,11 @@ def db_to_xml(city_name: str,
         # info will be the correct time node
         info = {}
 
-        # TODO select date and time
+        # select date and time
         if is_forecast:
             for timestamp in times:
                 f, t = timestamp['@from'], timestamp['@to']
-                # TODO select correct iteration based on data
+                # select correct node based on data
                 start = datetime.strptime(f.replace("T", " "), "%Y-%m-%d %H:%M:%S")
                 end = datetime.strptime(t.replace("T", " "), "%Y-%m-%d %H:%M:%S")
                 if start < date < end:
