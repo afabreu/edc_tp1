41780b17940a3a7e42243f018897cfd902dc7d27
André Alves <andr.alves@ua.pt>
2020-11-16 02:56:19 +0000

    updated forecast.html

--- app/static/xml/forecast.xsl
+++ app/static/xml/forecast.xsl
@@ -2,6 +2,19 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
     <xsl:output method="html" encoding="UTF-8" indent="yes"/>
     <xsl:template match="/">
+        <div class="row">
+            <div class="col-sm-2">
+                <img>
+                    <xsl:attribute name="src">http://openweathermap.org/img/wn/<xsl:value-of select="time/symbol/@var"/>@4x.png</xsl:attribute>
+                </img>
+            </div>
+            <div class="col-sm-2">
+                <br/>
+                <h5 class="text-warning"> <i class="fas fa-temperature-high"></i> <b> <xsl:value-of select="time/temperature/@max"/>°C</b> </h5>
+                <h1><xsl:value-of select="time/temperature/@value"/>°C</h1>
+                <h5 class="text-info"> <i class="fas fa-temperature-low"></i> <b> <xsl:value-of select="time/temperature/@min"/>°C</b></h5>
+            </div>
+        </div>
         <table class="table">
             <tbody>
                 <tr>
--- app/views.py
+++ app/views.py
@@ -35,22 +35,22 @@ def home(request):
     location_str, location_id = get_local_id(location_str)
     # create or open db
     database()
-    weather_data = basex_actions.db_to_xml(location_str)
+#    weather_data = basex_actions.db_to_xml(location_str)
     tparams = {
         'title': f'Meteorologia - {datetime.now().day}/{datetime.now().month}',
         'year': datetime.now().year,
         'location': location_str,
         'location_id': location_id,
-        'symbol': f"{weather_data['var']}: {weather_data['name']}",
-        'precipitation': f"{weather_data['precipitation']['probability']*100}%",
-        'windDirection': weather_data['windDirection']['name'],
-        'windSpeed': f"{weather_data['mps']} {weather_data['unit']}",
-        'temperature': f"{weather_data['value']} {weather_data['unit']}",
-        'feels_like': f"{weather_data['value']} {weather_data['unit']}",
-        'pressure': f"{weather_data['value']} {weather_data['unit']}",
-        'humidity': f"{weather_data['value']} {weather_data['unit']}",
-        'clouds': f"{weather_data['value']}, {weather_data['all']} {weather_data['unit']}",
-        'visibility': weather_data['value'],
+       # 'symbol': f"{weather_data['var']}: {weather_data['name']}",
+       # 'precipitation': f"{weather_data['precipitation']['probability']*100}%",
+       # 'windDirection': weather_data['windDirection']['name'],
+       # 'windSpeed': f"{weather_data['mps']} {weather_data['unit']}",
+       # 'temperature': f"{weather_data['value']} {weather_data['unit']}",
+       # 'feels_like': f"{weather_data['value']} {weather_data['unit']}",
+       # 'pressure': f"{weather_data['value']} {weather_data['unit']}",
+       # 'humidity': f"{weather_data['value']} {weather_data['unit']}",
+       # 'clouds': f"{weather_data['value']}, {weather_data['all']} {weather_data['unit']}",
+       # 'visibility': weather_data['value'],
     }
     return render(request, 'index.html', tparams)
 
@@ -61,68 +61,68 @@ def forecast(request, local_id):
     now -= remove_min_sec  # Dia e Hora(apenas hora) atuais
     hour_of_today = now.hour
     remove_hour = timedelta(hours=hour_of_today)
-    today = now - remove_hour  # Dia de hoje
+    submit_day = now - remove_hour  # Dia de hoje (sem horas)
     hour_of_today -= (hour_of_today % 3)  # Hora atual divisível por 3 anterior
-    now = today + timedelta(hours=hour_of_today)
+    now = submit_day + timedelta(hours=hour_of_today)
     if 'inputDia' in request.POST and 'inputHora' in request.POST:
         dia = request.POST['inputDia']
         hora = request.POST['inputHora']
 
-        sum_day = -1
-        if dia == "Hoje":
-            sum_day = timedelta(days=0)
-        elif dia == "Amanhã":
-            sum_day = timedelta(days=1)
+        if dia == "Amanhã":
+            sum_day = 1
         elif dia == "Daqui a 2 dias":
-            sum_day = timedelta(days=2)
+            sum_day = 2
         elif dia == "Daqui a 3 dias":
-            sum_day = timedelta(days=3)
+            sum_day = 3
         elif dia == "Daqui a 4 dias":
-            sum_day = timedelta(days=4)
+            sum_day = 4
         elif dia == "Daqui a 5 dias":
-            sum_day = timedelta(days=5)
+            sum_day = 5
+        else:
+            sum_day = 0
 
-        today += sum_day
+        submit_day += timedelta(days=sum_day)
 
-        sum_hour = -1
         if hora == "00:00 - 03:00":
-            sum_hour = timedelta(hours=0)
+            sum_hour = 0
         elif hora == "03:00 - 06:00":
-            sum_hour = timedelta(hours=3)
+            sum_hour = 3
         elif hora == "06:00 - 09:00":
-            sum_hour = timedelta(hours=6)
+            sum_hour = 6
         elif hora == "09:00 - 12:00":
-            sum_hour = timedelta(hours=9)
+            sum_hour = 9
         elif hora == "12:00 - 15:00":
-            sum_hour = timedelta(hours=12)
+            sum_hour = 12
         elif hora == "15:00 - 18:00":
-            sum_hour = timedelta(hours=15)
+            sum_hour = 15
         elif hora == "18:00 - 21:00":
-            sum_hour = timedelta(hours=18)
+            sum_hour = 18
         elif hora == "21:00 - 00:00":
-            sum_hour = timedelta(hours=21)
+            sum_hour = 21
 
-        today += sum_hour
+        submit_day += timedelta(hours=sum_hour)
 
-        if (today - now).days >= 5:
+        difference_data = submit_day - now
+
+        if difference_data.days >= 5 or difference_data.days < 0:
             messages.warning(request, 'Selecione apenas até 5 dias.')
-            today = now
+            submit_day = now
 
     else:
-        today = now
+        submit_day = now
 
     location_str, location_id = local_str(local_id)
 
     # create or open db
     database()
 
-    # TODO f1 xml = basex_actions.db_to_xml(db_name, location_str, today)
+    # TODO f1 xml = basex_actions.db_to_xml(db_name, location_str, submit_day)
     # TODO dict_city = data_dict(xml)
     # TODO tparams getting info from dict_city
 
     query = f"""for $a in collection('FiveDayForecast')//weatherdata 
                 for $b in $a/forecast/time 
-                    where $a//name = "Aveiro" and $b/@from = "{today.isoformat()}"
+                    where $a//name = "Aveiro" and $b/@from = "{submit_day.isoformat()}"
                     return $b"""
     query2 = session.query(query)
     res = query2.execute()
@@ -133,17 +133,21 @@ def forecast(request, local_id):
     html = transform(root)
 
     context = {
-        'title': f'Meteorologia - {today.day}/{today.month} - {today.hour}:00',
+        'title': f'Meteorologia - {submit_day.day}/{submit_day.month} - {submit_day.hour}:00',
         'year': datetime.now().year,
-        'location': f'{location_str} - {location_id}',
-        'symbol': "04d",
-        'temp_inicio': today.hour,
-        'temp_fim': today.hour + 3,
-        'temp_dia': today.day,
+        'location': f'{location_str}',
+        'temp_inicio': submit_day.hour,
+        'temp_fim': submit_day.hour + 3,
+        'temp_dia': submit_day.day,
         'content': html
     }
 
     return render(request, 'forecast.html', context)
+
+
+def img(request, var):
+    image = f'img/{var}.png'
+    return os.path.join(edc_tp1.settings.STATIC_URL, image)
 
 
 def database(name: str = "FiveDayForecast"):
