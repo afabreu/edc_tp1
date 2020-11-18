93c4883cce6ebe9faaa956a6cffceb10da9afff9
Alexandre Abreu <alexandre.abreu@ua.pt>
2020-11-18 11:31:40 +0000

    Insert new cities forecast into basex

--- app/views.py
+++ app/views.py
@@ -44,9 +44,21 @@ def home(request):
     database()
 
     root_current = basex_actions.current_weather(location_str)
+
     xslt_file = etree.parse(f"{edc_tp1.settings.XML_URL}weather.xsl")
     transform = etree.XSLT(xslt_file)
     html = transform(root_current)
+
+    root_forecast = basex_actions.api_call(location_id, to_string=True)
+
+    if "<?xml" in root_forecast:
+        root_forecast = root_forecast[39:]
+
+    query = f"""let $d := doc('FiveDayForecast')
+                return insert node {root_forecast} as last into $d/FiveDayForecast """
+
+    query2 = session.query(query)
+    query2.execute()
 
     context = {
         'title': f'Meteorologia - {datetime.now().day}/{datetime.now().month}',
